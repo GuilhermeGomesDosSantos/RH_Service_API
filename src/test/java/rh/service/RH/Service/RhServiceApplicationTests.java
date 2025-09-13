498b30fce23e578e933eb5ae00534787436e0cf7
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-09-12 21:14:56 -0300

    Iniciando Projeto API RH_Service

---
+++ src/test/java/rh/service/RH/Service/RhServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rh.service.RH.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
