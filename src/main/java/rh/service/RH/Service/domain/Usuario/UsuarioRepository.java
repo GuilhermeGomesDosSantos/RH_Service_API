e8fd77e72d9c5a48cf7c81110401681e2a9272fe
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-09-27 07:46:14 -0300

    Realizando configuração de segurança

---
+++ src/main/java/rh/service/RH/Service/domain/Usuario/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package rh.service.RH.Service.domain.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<usuario, Long> {
+
+    UserDetails findByLogin(String username);
+}
